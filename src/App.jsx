3aab6f2ba03c7b2c5e142fc3645b77d6c09e5e4f
dieswae <diegooare11@gmail.com>
2023-07-17 18:01:20 -0300

    cambia el nombre

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <header>
-        <h1>diechollo$</h1>
+        <h1>ecommerce</h1>
         <nav>
           <ul>
             <li><a href='/'>Home</a></li>
